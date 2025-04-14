ae164e6ee46a34c4d0bfa06b2c79df69c2adcc58
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-04-14 20:08:35 +0100

    testing deployment

--- infras/terraform/cppv2-integration/main_us.tf
+++ infras/terraform/cppv2-integration/main_us.tf
@@ -156,7 +156,7 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration" {
 # }
 
 resource "aws_api_gateway_integration_response" "userplatform_cpp_apigateway_s3_integration_response" {
-  for_each    = local.route_path
+  for_each = local.route_path
 
   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api.id
   resource_id = aws_api_gateway_resource.userplatform_cpp_api_resources[each.key].id
