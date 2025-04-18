84979578c55998e6d37a70a675e55e7f03fb4d4e
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2025-04-18 16:07:39 +0100

    testing cross region resources

--- infras/terraform/cppv2-integration/locals.tf
+++ infras/terraform/cppv2-integration/locals.tf
@@ -10,13 +10,13 @@ locals {
       region     = "eu-central-1",
       route_path = var.route_path["eu"]
       bucket     = var.userplatform_s3_bucket["eu"],
-      #   event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn,
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn,
     },
     ap = {
       region     = "ap-northeast-1",
       route_path = var.route_path["ap"]
       bucket     = var.userplatform_s3_bucket["ap"],
-      #   event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn,
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn,
     }
   }
 }
