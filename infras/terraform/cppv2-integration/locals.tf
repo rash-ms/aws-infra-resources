1ca01d7a7e5aa8987f59d5e96d76f3602ccce30a
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-18 14:51:34 +0100

    commit chnages

--- infras/terraform/cppv2-integration/locals.tf
+++ infras/terraform/cppv2-integration/locals.tf
@@ -1,23 +1,23 @@
-# locals {
-#   route_configs = {
-#     us = {
-#       region     = "us-east-1",
-#       route_path = var.route_path["us"],
-#       bucket     = var.userplatform_s3_bucket["us"],
-#       event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_us.arn
-#
-#     },
-#     eu = {
-#       region     = "eu-central-1",
-#       route_path = var.route_path["eu"],
-#       bucket     = var.userplatform_s3_bucket["eu"],
-#       event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn,
-#     },
-#     ap = {
-#       region     = "ap-northeast-1",
-#       route_path = var.route_path["ap"],
-#       bucket     = var.userplatform_s3_bucket["ap"],
-#       event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn,
-#     }
-#   }
-# }
+locals {
+  route_configs = {
+    us = {
+      region     = "us-east-1",
+      route_path = var.route_path["us"],
+      bucket     = var.userplatform_s3_bucket["us"],
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_us.arn
+
+    },
+    eu = {
+      region     = "eu-central-1",
+      route_path = var.route_path["eu"],
+      bucket     = var.userplatform_s3_bucket["eu"],
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn,
+    },
+    ap = {
+      region     = "ap-northeast-1",
+      route_path = var.route_path["ap"],
+      bucket     = var.userplatform_s3_bucket["ap"],
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn,
+    }
+  }
+}
