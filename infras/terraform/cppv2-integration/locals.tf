b3516f416536f6f0e01d2da90c9246b1602980a6
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-19 20:45:28 +0100

    commit chnages

--- infras/terraform/cppv2-integration/locals.tf
+++ infras/terraform/cppv2-integration/locals.tf
@@ -4,20 +4,20 @@ locals {
       region     = "us-east-1",
       route_path = var.route_path["us"],
       bucket     = var.userplatform_s3_bucket["us"],
-      # event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_us.arn
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_us.arn
 
     },
     eu = {
       region     = "eu-central-1",
       route_path = var.route_path["eu"],
       bucket     = var.userplatform_s3_bucket["eu"],
-      # event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_eu.arn
     },
     ap = {
       region     = "ap-northeast-1",
       route_path = var.route_path["ap"],
       bucket     = var.userplatform_s3_bucket["ap"],
-      # event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn
+      event_bus  = aws_cloudwatch_event_bus.userplatform_cpp_event_bus_ap.arn
     }
   }
 }
