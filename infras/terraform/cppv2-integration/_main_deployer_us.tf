c3eaab7dce42a8d61e852dc1874144047833980f
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-21 23:48:53 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_us.tf
+++ infras/terraform/cppv2-integration/_main_deployer_us.tf
@@ -394,12 +394,12 @@ resource "aws_api_gateway_stage" "userplatform_cpp_api_stage_us" {
   xray_tracing_enabled = true
   depends_on = [
     aws_api_gateway_account.userplatform_cpp_api_account_settings_us,
-    aws_api_gateway_integration.userplatform_cpp_api_integration_us
+    # aws_api_gateway_integration.userplatform_cpp_api_integration_us
   ]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 }
 
 resource "aws_api_gateway_method_settings" "userplatform_cpp_apigateway_method_settings_us" {
