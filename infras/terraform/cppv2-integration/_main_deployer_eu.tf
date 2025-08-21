6b25f9b4a45dd29bcb914047366378e0c1ce4ddb
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-21 15:35:06 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_eu.tf
+++ infras/terraform/cppv2-integration/_main_deployer_eu.tf
@@ -154,13 +154,12 @@ resource "aws_api_gateway_deployment" "userplatform_cpp_api_deployment_eu" {
 
   triggers = {
     redeploy = sha1(jsonencode({
-      # uri                = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.uri
-      request_templates = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.request_templates
-      # request_parameters = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.request_parameters
-
-      # integration_http_method = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.integration_http_method
-      # credentials             = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.credentials
-      # passthrough_behavior    = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.passthrough_behavior
+      uri                     = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.uri
+      request_templates       = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.request_templates
+      request_parameters      = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.request_parameters
+      integration_http_method = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.integration_http_method
+      credentials             = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.credentials
+      passthrough_behavior    = aws_api_gateway_integration.userplatform_cpp_api_integration_eu.passthrough_behavior
     }))
   }
 
@@ -170,15 +169,14 @@ resource "aws_api_gateway_deployment" "userplatform_cpp_api_deployment_eu" {
   #   # redeploy = "sqs-migration-v2"
   # }
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 
   depends_on = [
     aws_api_gateway_integration_response.userplatform_cpp_apigateway_s3_integration_response_eu,
-
-    # aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
-    # aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_eu,
+    aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
+    aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_eu,
   ]
 }
 
