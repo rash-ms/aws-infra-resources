3bf9ac8c06a016d26e80adaadbb810f923f438a5
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-22 15:59:26 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_eu.tf
+++ infras/terraform/cppv2-integration/_main_deployer_eu.tf
@@ -81,90 +81,52 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_eu" {
   }
 }
 
-# resource "aws_api_gateway_integration_response" "userplatform_cpp_apigateway_s3_integration_response_eu" {
-#   provider    = aws.eu
-#   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
-#   resource_id = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
-#   http_method = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
-#   status_code = "200"
-#
-#   depends_on = [
-#     aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
-#     aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_eu
-#   ]
-#
-#   response_parameters = {
-#     "method.response.header.x-amz-request-id" = "integration.response.header.x-amz-request-id",
-#     "method.response.header.etag"             = "integration.response.header.ETag"
-#   }
-#
-#   response_templates = {
-#     "application/json" = ""
-#   }
-# }
-#
-# resource "aws_api_gateway_method_response" "userplatform_cpp_apigateway_s3_method_response_eu" {
-#   provider    = aws.eu
-#   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
-#   resource_id = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
-#   http_method = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
-#   status_code = "200"
-#
-#   response_parameters = {
-#     "method.response.header.x-amz-request-id" = true,
-#     "method.response.header.etag"             = true
-#   }
-#
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
-# }
 
 ## Method responses: declare allowed status codes
-# resource "aws_api_gateway_method_response" "userplatform_cpp_api_sqs_mthd_resp_eu" {
-#   provider = aws.eu
-#
-#   for_each    = local.responses
-#   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
-#   resource_id = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
-#   http_method = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
-#   status_code = each.key
-#
-#   response_parameters = {
-#     "method.response.header.x-amz-request-id" = true,
-#     "method.response.header.etag"             = true
-#   }
-#
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
-# }
-#
-# ## Integration responses: map SQS → client
-# resource "aws_api_gateway_integration_response" "userplatform_cpp_api_sqs_integration_resp_eu" {
-#   provider = aws.eu
-#
-#   for_each          = local.responses
-#   rest_api_id       = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
-#   resource_id       = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
-#   http_method       = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
-#   status_code       = aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu[each.key].status_code
-#   selection_pattern = try(each.value.selection_pattern, null)
-#
-#   depends_on = [
-#     aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
-#     aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu
-#   ]
-#
-#   response_parameters = {
-#     "method.response.header.x-amz-request-id" = "integration.response.header.x-amz-request-id",
-#     "method.response.header.etag"             = "integration.response.header.ETag"
-#   }
-#
-#   response_templates = {
-#     "application/json" = each.value.template
-#   }
-# }
+resource "aws_api_gateway_method_response" "userplatform_cpp_api_sqs_mthd_resp_eu" {
+  provider = aws.eu
+
+  for_each    = local.responses
+  rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
+  resource_id = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
+  http_method = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
+  status_code = each.key
+
+  response_parameters = {
+    "method.response.header.x-amz-request-id" = true,
+    "method.response.header.etag"             = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+## Integration responses: map SQS → client
+resource "aws_api_gateway_integration_response" "userplatform_cpp_api_sqs_integration_resp_eu" {
+  provider = aws.eu
+
+  for_each          = local.responses
+  rest_api_id       = aws_api_gateway_rest_api.userplatform_cpp_rest_api_eu.id
+  resource_id       = aws_api_gateway_resource.userplatform_cpp_api_resource_eu.id
+  http_method       = aws_api_gateway_method.userplatform_cpp_api_method_eu.http_method
+  status_code       = aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu[each.key].status_code
+  selection_pattern = try(each.value.selection_pattern, null)
+
+  depends_on = [
+    aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
+    aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu
+  ]
+
+  response_parameters = {
+    "method.response.header.x-amz-request-id" = "integration.response.header.x-amz-request-id",
+    "method.response.header.etag"             = "integration.response.header.ETag"
+  }
+
+  response_templates = {
+    "application/json" = each.value.template
+  }
+}
 
 
 resource "aws_api_gateway_api_key" "userplatform_cpp_api_key_eu" {
@@ -215,8 +177,8 @@ resource "aws_api_gateway_deployment" "userplatform_cpp_api_deployment_eu" {
 
   depends_on = [
     aws_api_gateway_integration.userplatform_cpp_api_integration_eu,
-    # aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu,
-    # aws_api_gateway_integration_response.userplatform_cpp_api_sqs_integration_resp_eu
+    aws_api_gateway_method_response.userplatform_cpp_api_sqs_mthd_resp_eu,
+    aws_api_gateway_integration_response.userplatform_cpp_api_sqs_integration_resp_eu
   ]
 
   triggers = {
