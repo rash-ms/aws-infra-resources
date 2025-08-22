df31e345fd3d58fdb2f27fa6cbb0c2928efb3fca
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-22 06:17:45 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_ap.tf
+++ infras/terraform/cppv2-integration/_main_deployer_ap.tf
@@ -9,27 +9,6 @@
 ## - Stage and deployment management
 ## --------------------------------------------------
 
-data "aws_sqs_queue" "userplatform_cppv2_sqs_ap" {
-  provider = aws.ap
-  name     = "userplatform_cppv2_sqs_ap"
-}
-
-data "aws_sqs_queue" "userplatform_cppv2_sqs_dlq_ap" {
-  provider = aws.ap
-  name     = "userplatform_cppv2_sqs_dlq_ap"
-}
-
-data "aws_lambda_function" "cppv2_sqs_lambda_firehose_ap" {
-  provider      = aws.ap
-  function_name = "cppv2_sqs_lambda_firehose_ap"
-}
-
-# Reference the existing bucket
-# data "aws_s3_bucket" "userplatform_bucket_ap" {
-#   bucket = local.route_configs["ap"].bucket
-# }
-
-
 resource "aws_api_gateway_rest_api" "userplatform_cpp_rest_api_ap" {
   provider    = aws.ap
   name        = "userplatform_cpp_rest_api_ap"
@@ -56,37 +35,44 @@ resource "aws_api_gateway_method" "userplatform_cpp_api_method_ap" {
   api_key_required = true
 }
 
-resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap_test" {
+resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
   provider                = aws.ap
   rest_api_id             = aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id
   resource_id             = aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id
   http_method             = aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method
   integration_http_method = "POST"
   type                    = "AWS"
-  # ARN format: arn:aws:apigateway:{region}:sqs:path/{account_id}/{queue_name}
-  # "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  # "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  uri         = "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  credentials = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+  uri                     = "arn:aws:apigateway:${local.route_configs["ap"].region}:events:path//"
+  credentials             = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
 
   # WHEN_NO_MATCH: Pass raw request if Content-Type doesn't match any template
   # WHEN_NO_TEMPLATES: Strict – if any template exists, Content-Type must match exactly
-  passthrough_behavior = "NEVER"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
 
-  request_parameters = {
-    "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
-  }
+  # request_templates = {
+  #   "application/json" = templatefile("${path.module}/templates/apigateway_reqst_template.tftpl", {
+  #     event_bus_arn = local.route_configs["ap"].event_bus
+  #     detail_type   = local.route_configs["ap"].route_path
+  #   })
+  # }
 
   request_templates = {
-    "application/json" = "Action=SendMessage&MessageBody=$input.body"
+    "application/json" = <<EOF
+#set($context.requestOverride.header.X-Amz-Target = "AWSEvents.PutEvents")
+#set($context.requestOverride.header.Content-Type = "application/x-amz-json-1.1")
+{
+  "Entries": [
+    {
+      "Source": "cpp-api-streamhook",
+      "DetailType": "${local.route_configs["ap"].route_path}",
+      "Detail": "$util.escapeJavaScript($input.body)",
+      "EventBusName": "${local.route_configs["ap"].event_bus}"
+    }
+  ]
+}
+EOF
   }
 }
-
-moved {
-  from = aws_api_gateway_integration.userplatform_cpp_api_integration_ap
-  to   = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test
-}
-
 
 resource "aws_api_gateway_integration_response" "userplatform_cpp_apigateway_s3_integration_response_ap" {
   provider    = aws.ap
@@ -96,7 +82,7 @@ resource "aws_api_gateway_integration_response" "userplatform_cpp_apigateway_s3_
   status_code = "200"
 
   depends_on = [
-    aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test,
+    aws_api_gateway_integration.userplatform_cpp_api_integration_ap,
     aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_ap
   ]
 
@@ -160,27 +146,17 @@ resource "aws_api_gateway_deployment" "userplatform_cpp_api_deployment_ap" {
   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id
 
   depends_on = [
-    aws_api_gateway_method.userplatform_cpp_api_method_ap,
-    aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test,
+    aws_api_gateway_integration.userplatform_cpp_api_integration_ap,
     aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_ap,
     aws_api_gateway_integration_response.userplatform_cpp_apigateway_s3_integration_response_ap
   ]
 
-  triggers = {
-    redeploy = sha1(jsonencode({
-      method                  = aws_api_gateway_method.userplatform_cpp_api_method_ap.id
-      uri                     = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.uri
-      request_templates       = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.request_templates
-      request_parameters      = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.request_parameters
-      integration_http_method = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.integration_http_method
-      credentials             = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.credentials
-      passthrough_behavior    = aws_api_gateway_integration.userplatform_cpp_api_integration_ap_test.passthrough_behavior
-    }))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  #   triggers = {
+  #     redeploy_tmpt_changes = sha1(templatefile("${path.module}/templates/apigateway_reqst_template.tftpl", {
+  #       event_bus_arn = local.route_configs["ap"].event_bus
+  #       detail_type   = local.route_configs["ap"].route_path
+  #     }))
+  #   }
 }
 
 resource "aws_api_gateway_stage" "userplatform_cpp_api_stage_ap" {
