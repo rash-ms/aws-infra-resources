fe3606ee4a797d1650a18fe401b98efe8b14dcc0
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-24 11:30:11 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_ap.tf
+++ infras/terraform/cppv2-integration/_main_deployer_ap.tf
@@ -10,7 +10,7 @@
 ## --------------------------------------------------
 
 locals {
-  force_redeploy_ap = "cppv2-release-v0.16"
+  force_redeploy_ap = "cppv2-release-v0.11"
 }
 
 data "aws_sqs_queue" "userplatform_cppv2_sqs_ap" {
@@ -74,14 +74,14 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
   type                    = "AWS"
 
   ## EVENTBRIDGE INTEGRATION
-  # uri         = "arn:aws:apigateway:${local.route_configs["ap"].region}:events:path//"
-  # credentials = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
-  # passthrough_behavior = "WHEN_NO_TEMPLATES"
+  uri                  = "arn:aws:apigateway:${local.route_configs["ap"].region}:events:path//"
+  credentials          = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
 
   ## SQS INTEGRATION
-  uri                  = "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  credentials          = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
-  passthrough_behavior = "NEVER"
+  # uri                  = "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
+  # credentials          = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+  # passthrough_behavior = "NEVER"
 
   request_parameters = {
     "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
@@ -89,22 +89,22 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
 
   request_templates = {
 
-    "application/json" = "Action=SendMessage&MessageBody=$input.body"
+    # "application/json" = "Action=SendMessage&MessageBody=$input.body"
 
-    #   "application/json" = <<EOF
-    # #set($context.requestOverride.header.X-Amz-Target = "AWSEvents.PutEvents")
-    # #set($context.requestOverride.header.Content-Type = "application/x-amz-json-1.1")
-    # {
-    #   "Entries": [
-    #     {
-    #       "Source": "cpp-api-streamhook",
-    #       "DetailType": "${local.route_configs["ap"].route_path}",
-    #       "Detail": "$util.escapeJavaScript($input.body)",
-    #       "EventBusName": "${local.route_configs["ap"].event_bus}"
-    #     }
-    #   ]
-    # }
-    # EOF
+    "application/json" = <<EOF
+    #set($context.requestOverride.header.X-Amz-Target = "AWSEvents.PutEvents")
+    #set($context.requestOverride.header.Content-Type = "application/x-amz-json-1.1")
+    {
+      "Entries": [
+        {
+          "Source": "cpp-api-streamhook",
+          "DetailType": "${local.route_configs["ap"].route_path}",
+          "Detail": "$util.escapeJavaScript($input.body)",
+          "EventBusName": "${local.route_configs["ap"].event_bus}"
+        }
+      ]
+    }
+    EOF
 
   }
 }
