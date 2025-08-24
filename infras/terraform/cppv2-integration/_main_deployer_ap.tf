47720a9dc21a8d64c32dad33e7f3619b8eae7264
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-24 11:26:23 +0100

    commit chnages

--- infras/terraform/cppv2-integration/_main_deployer_ap.tf
+++ infras/terraform/cppv2-integration/_main_deployer_ap.tf
@@ -10,7 +10,7 @@
 ## --------------------------------------------------
 
 locals {
-  force_redeploy_ap = "cppv2-release-v0.13"
+  force_redeploy_ap = "cppv2-release-v0.16"
 }
 
 data "aws_sqs_queue" "userplatform_cppv2_sqs_ap" {
@@ -59,6 +59,12 @@ resource "aws_api_gateway_method" "userplatform_cpp_api_method_ap" {
   api_key_required = true
 }
 
+# ARN format: arn:aws:apigateway:{region}:sqs:path/{account_id}/{queue_name}
+# "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
+# "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
+# WHEN_NO_MATCH: Pass raw request if Content-Type doesn't match any template
+# WHEN_NO_TEMPLATES: Strict – if any template exists, Content-Type must match exactly
+
 resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
   provider                = aws.ap
   rest_api_id             = aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id
@@ -67,14 +73,14 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
   integration_http_method = "POST"
   type                    = "AWS"
 
-  # ARN format: arn:aws:apigateway:{region}:sqs:path/{account_id}/{queue_name}
-  # "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  # "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  uri         = "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
-  credentials = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+  ## EVENTBRIDGE INTEGRATION
+  # uri         = "arn:aws:apigateway:${local.route_configs["ap"].region}:events:path//"
+  # credentials = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
+  # passthrough_behavior = "WHEN_NO_TEMPLATES"
 
-  # WHEN_NO_MATCH: Pass raw request if Content-Type doesn't match any template
-  # WHEN_NO_TEMPLATES: Strict – if any template exists, Content-Type must match exactly
+  ## SQS INTEGRATION
+  uri                  = "arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name}"
+  credentials          = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
   passthrough_behavior = "NEVER"
 
   request_parameters = {
@@ -82,34 +88,25 @@ resource "aws_api_gateway_integration" "userplatform_cpp_api_integration_ap" {
   }
 
   request_templates = {
+
     "application/json" = "Action=SendMessage&MessageBody=$input.body"
+
+    #   "application/json" = <<EOF
+    # #set($context.requestOverride.header.X-Amz-Target = "AWSEvents.PutEvents")
+    # #set($context.requestOverride.header.Content-Type = "application/x-amz-json-1.1")
+    # {
+    #   "Entries": [
+    #     {
+    #       "Source": "cpp-api-streamhook",
+    #       "DetailType": "${local.route_configs["ap"].route_path}",
+    #       "Detail": "$util.escapeJavaScript($input.body)",
+    #       "EventBusName": "${local.route_configs["ap"].event_bus}"
+    #     }
+    #   ]
+    # }
+    # EOF
+
   }
-
-
-  # uri         = "arn:aws:apigateway:${local.route_configs["ap"].region}:events:path//"
-  # credentials = aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn
-  #
-  # # WHEN_NO_MATCH: Pass raw request if Content-Type doesn't match any template
-  # # WHEN_NO_TEMPLATES: Strict – if any template exists, Content-Type must match exactly
-  # passthrough_behavior = "WHEN_NO_TEMPLATES"
-  #
-  # request_templates = {
-  #   "application/json" = <<EOF
-  # #set($context.requestOverride.header.X-Amz-Target = "AWSEvents.PutEvents")
-  # #set($context.requestOverride.header.Content-Type = "application/x-amz-json-1.1")
-  # {
-  #   "Entries": [
-  #     {
-  #       "Source": "cpp-api-streamhook",
-  #       "DetailType": "${local.route_configs["ap"].route_path}",
-  #       "Detail": "$util.escapeJavaScript($input.body)",
-  #       "EventBusName": "${local.route_configs["ap"].event_bus}"
-  #     }
-  #   ]
-  # }
-  # EOF
-  # }
-
 }
 
 
@@ -205,19 +202,28 @@ resource "aws_api_gateway_usage_plan_key" "userplatform_cpp_api_usage_plan_key_a
 }
 
 
+resource "null_resource" "wait_after_integration_ap" {
+  depends_on = [
+    aws_api_gateway_integration.userplatform_cpp_api_integration_ap
+  ]
+
+  provisioner "local-exec" {
+    command     = "echo 'Waiting 60s for API Gateway integration to settle...' && sleep 60"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+
 resource "aws_api_gateway_deployment" "userplatform_cpp_api_deployment_ap" {
   provider    = aws.ap
   rest_api_id = aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id
 
   depends_on = [
+    null_resource.wait_after_integration_ap,
     aws_api_gateway_integration.userplatform_cpp_api_integration_ap,
     aws_api_gateway_method_response.userplatform_cpp_apigateway_s3_method_response_ap,
     aws_api_gateway_integration_response.userplatform_cpp_apigateway_s3_integration_response_ap
   ]
-
-  # depends_on = [
-  #   null_resource.force_put_sqs_integration_ap
-  # ]
 
   triggers = {
     redeploy = local.force_redeploy_ap
