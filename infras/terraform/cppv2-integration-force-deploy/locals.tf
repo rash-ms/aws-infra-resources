805e89dd3a38d509f9c12d4bb09b1954e47cab01
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-24 17:30:38 +0100

    commit chnages

---
+++ infras/terraform/cppv2-integration-force-deploy/locals.tf
@@ -0,0 +1,55 @@
+locals {
+  route_configs = {
+    us = {
+      region     = "us-east-1",
+      route_path = var.route_path["us"],
+      bucket     = var.userplatform_s3_bucket["us"]
+
+    },
+    eu = {
+      region     = "eu-central-1",
+      route_path = var.route_path["eu"],
+      bucket     = var.userplatform_s3_bucket["eu"]
+    },
+    ap = {
+      region     = "ap-northeast-1",
+      route_path = var.route_path["ap"],
+      bucket     = var.userplatform_s3_bucket["ap"]
+    }
+  }
+}
+
+
+locals {
+  sqs_integration_responses = {
+    "200" = {
+      selection_pattern = null # "2\\d{2}"
+      # success → no selection_pattern
+      template = <<EOF
+{
+  "messageId": "$input.path('$.SendMessageResponse.SendMessageResult.MessageId')"
+}
+EOF
+    }
+    "400" = {
+      selection_pattern = "4\\d{2}"
+      template          = <<EOF
+{
+  "error":"bad_request_SQS",
+  "status":"$context.integration.status",
+  "details":"$util.escapeJavaScript($input.body)"
+}
+EOF
+    }
+    "500" = {
+      selection_pattern = "5\\d{2}"
+      template          = <<EOF
+{
+  "error":"internal_failure_SQS",
+  "status":"$context.integration.status",
+  "details":"$util.escapeJavaScript($input.body)"
+}
+EOF
+    }
+  }
+}
