a71f8dc16de5272f6f82062310f7d4a18f5c6afb
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-24 09:40:30 +0100

    commit chnages

--- infras/terraform/cppv2-integration/redeploy_trigger_v1.tf
+++ infras/terraform/cppv2-integration/redeploy_trigger_v1.tf
@@ -61,29 +61,29 @@ resource "null_resource" "force_put_sqs_integration_eu" {
 ################################################################  APAC  ################################################################
 ################################################################      ################################################################
 
-resource "null_resource" "force_put_sqs_integration_ap" {
-  depends_on = [
-    aws_api_gateway_stage.userplatform_cpp_api_stage_ap,
-    aws_api_gateway_integration.userplatform_cpp_api_integration_ap,
-    aws_api_gateway_deployment.userplatform_cpp_api_deployment_ap
-  ]
-
-  triggers = {
-    redeploy = local.force_redeploy_ap
-  }
-
-  provisioner "local-exec" {
-    command     = <<-EOT
-      sleep 10
-
-      # Force new deployment to stage
-      aws apigateway create-deployment \
-        --region ${local.route_configs["ap"].region} \
-        --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-        --stage-name ${aws_api_gateway_stage.userplatform_cpp_api_stage_ap.stage_name} \
-        --description "Auto-redeploy after updating integration to SQS"
-
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
+# resource "null_resource" "force_put_sqs_integration_ap" {
+#   depends_on = [
+#     aws_api_gateway_stage.userplatform_cpp_api_stage_ap,
+#     aws_api_gateway_integration.userplatform_cpp_api_integration_ap,
+#     aws_api_gateway_deployment.userplatform_cpp_api_deployment_ap
+#   ]
+#
+#   triggers = {
+#     redeploy = local.force_redeploy_ap
+#   }
+#
+#   provisioner "local-exec" {
+#     command     = <<-EOT
+#       sleep 10
+#
+#       # Force new deployment to stage
+#       aws apigateway create-deployment \
+#         --region ${local.route_configs["ap"].region} \
+#         --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#         --stage-name ${aws_api_gateway_stage.userplatform_cpp_api_stage_ap.stage_name} \
+#         --description "Auto-redeploy after updating integration to SQS"
+#
+#     EOT
+#     interpreter = ["/bin/bash", "-c"]
+#   }
+# }
