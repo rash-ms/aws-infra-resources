165f7013cc440e1669b8033cb0bcf4f9ede6cbce
mujadeniji-cn <mujeeb.adeniji@condenast.com>
2025-08-27 12:37:13 +0100

    commit chnages

--- infras/terraform/cppv2-integration/redeploy_trigger_v1.tf
+++ infras/terraform/cppv2-integration/redeploy_trigger_v1.tf
@@ -163,90 +163,88 @@
 # ################################################################  APAC  ################################################################
 # ################################################################      ################################################################
 
-resource "null_resource" "force_put_sqs_integration_ap" {
-  depends_on = [
-    aws_api_gateway_stage.userplatform_cpp_api_stage_ap,
-    aws_api_gateway_deployment.userplatform_cpp_api_deployment_ap
-  ]
-
-  triggers = {
-    redeploy = local.force_redeploy_ap
-  }
-
-  provisioner "local-exec" {
-    command     = <<-EOT
-      aws apigateway put-integration \
-        --region ${local.route_configs["ap"].region} \
-        --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-        --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
-        --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
-        --type ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.type} \
-        --integration-http-method ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.integration_http_method} \
-        --uri arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name} \
-        --credentials ${aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn} \
-        --passthrough-behavior ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.passthrough_behavior} \
-        --request-parameters '{"integration.request.header.Content-Type":"'\''application/x-www-form-urlencoded'\''"}' \
-        --request-templates '{"application/json":"Action=SendMessage&MessageBody=$input.body"}'
-
-      # Loop through response configs
-      %{for code, cfg in local.sqs_integration_responses~}
-
-      # Upsert Method Responses
-      echo "Ensuring MethodResponse for status ${code}..."
-      if aws apigateway get-method-response \
-        --region ${local.route_configs["ap"].region} \
-        --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-        --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
-        --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
-        --status-code ${code} >/dev/null 2>&1; then
-
-        echo "Updating MethodResponse ${code}"
-        aws apigateway update-method-response \
-          --region ${local.route_configs["ap"].region} \
-          --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-          --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
-          --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
-          --status-code ${code} \
-          --patch-operations \
-            op=replace,path=/responseModels/application~1json,value=Empty \
-            op=add,path=/responseParameters/method.response.header.x-amz-request-id,value=true \
-            op=add,path=/responseParameters/method.response.header.etag,value=true
-      else
-        echo "Creating MethodResponse ${code}"
-        aws apigateway put-method-response \
-          --region ${local.route_configs["ap"].region} \
-          --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-          --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
-          --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
-          --status-code ${code} \
-          --response-models '{"application/json":"Empty"}' \
-          --response-parameters '{"method.response.header.x-amz-request-id":true,"method.response.header.etag":true}'
-      fi
-
-      aws apigateway put-integration-response \
-        --region ${local.route_configs["ap"].region} \
-        --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-        --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
-        --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
-        --status-code ${code} \
-        --response-parameters '{"method.response.header.x-amz-request-id":"integration.response.header.x-amz-request-id","method.response.header.etag":"integration.response.header.ETag"}' \
-        %{if try(cfg.selection_pattern, null) != null}--selection-pattern "${cfg.selection_pattern}" %{endif} \
-        --response-templates '${jsonencode({ "application/json" = cfg.template })}'
-
-      %{endfor~}
-
-      sleep 10
-
-      # Force new deployment to stage
-      aws apigateway create-deployment \
-        --region ${local.route_configs["ap"].region} \
-        --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
-        --stage-name ${var.stage_name} \
-        --description "Auto-redeploy after updating integration to SQS"
-
-    EOT
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
-
+# resource "null_resource" "force_put_sqs_integration_ap" {
+#   depends_on = [
+#     aws_api_gateway_stage.userplatform_cpp_api_stage_ap,
+#     aws_api_gateway_deployment.userplatform_cpp_api_deployment_ap
+#   ]
+#
+#   triggers = {
+#     redeploy = local.force_redeploy_ap
+#   }
+#
+#   provisioner "local-exec" {
+#     command     = <<-EOT
+#       aws apigateway put-integration \
+#         --region ${local.route_configs["ap"].region} \
+#         --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#         --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
+#         --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
+#         --type ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.type} \
+#         --integration-http-method ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.integration_http_method} \
+#         --uri arn:aws:apigateway:${local.route_configs["ap"].region}:sqs:path/${var.account_id}/${data.aws_sqs_queue.userplatform_cppv2_sqs_ap.name} \
+#         --credentials ${aws_iam_role.cpp_integration_apigw_evtbridge_firehose_logs_role.arn} \
+#         --passthrough-behavior ${aws_api_gateway_integration.userplatform_cpp_api_integration_ap.passthrough_behavior} \
+#         --request-parameters '{"integration.request.header.Content-Type":"'\''application/x-www-form-urlencoded'\''"}' \
+#         --request-templates '{"application/json":"Action=SendMessage&MessageBody=$input.body"}'
+#
+#       # Loop through response configs
+#       %{for code, cfg in local.sqs_integration_responses~}
+#
+#       # Upsert Method Responses
+#       echo "Ensuring MethodResponse for status ${code}..."
+#       if aws apigateway get-method-response \
+#         --region ${local.route_configs["ap"].region} \
+#         --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#         --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
+#         --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
+#         --status-code ${code} >/dev/null 2>&1; then
+#
+#         echo "Updating MethodResponse ${code}"
+#         aws apigateway update-method-response \
+#           --region ${local.route_configs["ap"].region} \
+#           --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#           --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
+#           --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
+#           --status-code ${code} \
+#           --patch-operations \
+#             op=replace,path=/responseModels/application~1json,value=Empty \
+#             op=add,path=/responseParameters/method.response.header.x-amz-request-id,value=true \
+#             op=add,path=/responseParameters/method.response.header.etag,value=true
+#       else
+#         echo "Creating MethodResponse ${code}"
+#         aws apigateway put-method-response \
+#           --region ${local.route_configs["ap"].region} \
+#           --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#           --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
+#           --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
+#           --status-code ${code} \
+#           --response-models '{"application/json":"Empty"}' \
+#           --response-parameters '{"method.response.header.x-amz-request-id":true,"method.response.header.etag":true}'
+#       fi
+#
+#       aws apigateway put-integration-response \
+#         --region ${local.route_configs["ap"].region} \
+#         --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#         --resource-id ${aws_api_gateway_resource.userplatform_cpp_api_resource_ap.id} \
+#         --http-method ${aws_api_gateway_method.userplatform_cpp_api_method_ap.http_method} \
+#         --status-code ${code} \
+#         --response-parameters '{"method.response.header.x-amz-request-id":"integration.response.header.x-amz-request-id","method.response.header.etag":"integration.response.header.ETag"}' \
+#         %{if try(cfg.selection_pattern, null) != null}--selection-pattern "${cfg.selection_pattern}" %{endif} \
 #         --response-templates '${jsonencode({ "application/json" = cfg.template })}'
+#
+#       %{endfor~}
+#
+#       sleep 10
+#
+#       # Force new deployment to stage
+#       aws apigateway create-deployment \
+#         --region ${local.route_configs["ap"].region} \
+#         --rest-api-id ${aws_api_gateway_rest_api.userplatform_cpp_rest_api_ap.id} \
+#         --stage-name ${var.stage_name} \
+#         --description "Auto-redeploy after updating integration to SQS"
+#
+#     EOT
+#     interpreter = ["/bin/bash", "-c"]
+#   }
+# }
